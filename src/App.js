822f988ccaa90859a39e3708dc9a812130986590
Adrian <pogorzelski.adrian@gmail.com>
2022-09-18 00:15:33 +0200

    Build?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1>Reddit Client: A Codecamy React/Redux project</h1>
+    <reddit-client>
+      <header>
+        <h1>Reddit Client: A Codecamy React/Redux project</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>Menu item</li>
+          <li>Menu item</li>
+          <li>Menu item</li>
+          <li>Menu item</li>
+        </ul>
+      </nav>
+      <main>
+        <p>Content</p>
+      </main>
+    </reddit-client>
   );
 }
 
