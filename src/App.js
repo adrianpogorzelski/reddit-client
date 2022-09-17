7098ff3af0706d20a5b2e4c352d1cc1c9f7499ab
a.pogorzelski <a.pogorzelski@ocado.com>
2022-09-17 15:56:41 +0200

    Fix?

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <h1>Reddit Client: A Codecamy React/Redux project</h1>
+    <p>This will be a React/Redux app</p>
   );
 }
 
