d08aa026d2a6fee8c033a7d2ebddced10b6cae5f
a.pogorzelski <a.pogorzelski@ocado.com>
2022-09-19 16:59:43 +0200

    Added README.md

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
-  return (
-    <reddit-client>
+    const [content, setContent] = useState(null)
+    const fetchRedditData = async () => {
+        const response = await fetch('https://www.reddit.com/r/cute.json')
+            .then((response) => response.json())
+        return response
+    }
+
+    fetchRedditData().then(dataObject => setContent(JSON.stringify(dataObject)))
+
+    return (
+    <client-for-reddit>
       <header>
         <h1>Reddit Client: A Codecamy React/Redux project</h1>
       </header>
@@ -15,9 +25,11 @@ function App() {
         </ul>
       </nav>
       <main>
-        <p>Content</p>
+        <h2>Content:</h2>
+          <p>Here is the JSON:</p>
+          <p>{content}</p>
       </main>
-    </reddit-client>
+    </client-for-reddit>
   );
 }
 
